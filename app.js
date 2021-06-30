3115bb98dd01431015787ce23172f6b57f3ab453
Lauren Libretti <lalibretti@yahoo.com>
2021-06-30 16:09:36 -0400

    updates sql/routes/api calls

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
