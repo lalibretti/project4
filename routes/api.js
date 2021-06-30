0ff6cc0bd23d8eeb49459ed2ef25b20ef2089baa
Lauren Libretti <lalibretti@yahoo.com>
2021-06-29 23:00:21 -0400

    working app
    db working & get/post functioning

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,33 @@
 var express = require ('express'); 
 var router = express.Router(); 
+const yahooStockPrices = require('yahoo-stock-prices')
+const {Portfolio, Wallet} = require('../lib/models');
 
-router.get('/test', function(require,result,next){
-    result.json{api:true}
+//Yahoo stock API
+router.get('/search/:symbol', async function(req, res, next) {
+    console.log('the req.params', req.params)
+    const data = await yahooStockPrices.getCurrentData(req.params.symbol);
+    res.json(data)
 });
 
-module.exports=router;
+router.get('/portfolio', async function(req, res, next) {
+    let items = await Portfolio.findAll({});
+    res.json(items);
+});
+
+router.post('/portfolio', async function(req, res, next) {
+    console.log('.the req.body is', req.body)
+    let item = await Portfolio.create(req.body)
+
+    res.json(item)
+});
+
+router.delete('/portfolio/:id', function(req, res, next) {
+    res.json({deleteRowInTheTable: true})
+});
+
+router.get('/test', function(req,result,next){
+    result.json ({api:true})
+});
+
+module.exports = router;
